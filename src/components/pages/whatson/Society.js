d18acd9dd381cb34c176e836e5a06a78af829add
Chun-Hei Lam <chun.lam18@imperial.ac.uk>
2024-01-14 23:16:46 +0000

    update society page

--- src/components/pages/whatson/Society.js
+++ src/components/pages/whatson/Society.js
@@ -20,8 +20,7 @@ const Society = () => {
   const getSociety = async () => {
     const BaseURL = "https://samuelchlam.herokuapp.com/api";
     const resSoc = await axios.get(`${BaseURL}/societies?filters[abbreviation][$eq]=${abbreviation}` + 
-      "&populate[wcrContacts][fields][0]=username&populate[wcrContacts][fields][1]=preferredName" + 
-      "&populate[prevCommContacts][fields][0]=username&populate[prevCommContacts][fields][1]=preferredName" + 
+      "&populate[profiles][fields][0]=username&populate[profiles][fields][1]=preferredName" + 
       "&populate[banner][fields][0]=url"
       ).then().catch((e) => console.log(e));
     setSociety(resSoc.data.data[0].attributes);
@@ -39,13 +38,18 @@ const Society = () => {
         <Markdown>{society.regularSchedule}</Markdown>
       </ContentBlock>
       <ContentBlock title="Contacts">
-        <h2>MCR Contacts</h2>
-        <ul>
-          {society.wcrContacts && society.wcrContacts.data.map((item, idx) => <li key={idx}><Link to={`/profile/${item.attributes.username}`}>{item.attributes.preferredName}</Link></li>)}
-          {society.prevCommContacts && society.prevCommContacts.data.map((item, idx) => <li key={idx}>{item.attributes.preferredName}</li>)}
-        </ul>
-        <h2>Other Contacts</h2>
-        <Markdown>{society.otherContacts}</Markdown>
+        <div className="u-gridPic-2">
+          <div>
+            <h2>MCR Contacts</h2>
+            <ul>
+              {society.profiles && society.profiles.data.map((item, idx) => <li key={idx}><Link to={`/profile/${item.attributes.username}`}>{item.attributes.preferredName}</Link></li>)}
+            </ul>
+          </div>
+          <div>
+            <h2>Other Contacts</h2>
+            <Markdown>{society.otherContacts}</Markdown>
+          </div>
+        </div>
       </ContentBlock>
     </>
   );
